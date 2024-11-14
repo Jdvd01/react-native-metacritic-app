7e3f9d787ff621a1c83c6b9f72dc2f73b56599ed
Jose Velasquez <velasquezdenorajose@gmail.com>
2024-11-14 01:29:38 -0400

    🚧 Data from the API and rendering

--- App.js
+++ App.js
@@ -1,20 +1,67 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { StyleSheet, Text, View, Pressable, Image } from 'react-native';
+import { getCharacters } from './lib/api';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	const [characters, setCharacters] = useState([])
+
+	useEffect(() => {
+		getCharacters().then((characters) => {
+			setCharacters(characters)
+		})
+	}, [])
+
+	return (
+		<View style={styles.container}>
+			<StatusBar style="light" />
+			
+			{characters.results?.map((character) => (
+				<View key={character.id} style={styles.card}>
+					<Image 
+						source={{ uri: character.image }}
+						style={styles.image}
+					/>
+					<Text style={styles.title}>{character.name}</Text>
+					<Text style={styles.description}>{character.species}</Text>
+					<Text style={styles.status}>{character.status}</Text>
+				</View>
+			)) }
+
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		color: '#fff',
+		flex: 1,
+		backgroundColor: '#000',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	card: {
+		marginBottom:10
+	},
+	image:{
+		width: 100,
+		height: 100,
+		borderRadius: 10
+	},
+	title: {
+		fontSize: 20,
+		fontWeight: 'bold',
+		marginBottom: 10,
+		color: '#fff'
+	},
+	description: {
+		fontSize: 16,
+		color: '#fff'
+	},
+	status:{
+		fontSize: 16,
+		color: '#ccc',
+		marginTop:10
+
+	}
 });
