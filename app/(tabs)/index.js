d22a9c869a7ece7298878683a44a58d213b0ef16
Jose Velasquez <velasquezdenorajose@gmail.com>
2024-11-17 19:47:59 -0400

    🔥 Removes unused StatusBar

--- app/(tabs)/index.js
+++ app/(tabs)/index.js
@@ -5,7 +5,6 @@ import Screen from "../../components/Screen";
 export default function Index() {
     return (
         <Screen>
-            <StatusBar style="light" />
             <Main />
         </Screen>
     )
