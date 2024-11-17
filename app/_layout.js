533c9dffef12225aa206bba7d536697b33a6c720
Jose Velasquez <velasquezdenorajose@gmail.com>
2024-11-17 19:36:06 -0400

    ✨ Adds Status bar

--- app/_layout.js
+++ app/_layout.js
@@ -2,10 +2,12 @@ import { Stack } from "expo-router"
 import { View } from "react-native"
 import { Logo } from "../components/Icons"
 import '../global.css'
+import { StatusBar } from "expo-status-bar"
 
 export default function Layout() {
     return (
         <View className='flex-1'>
+            <StatusBar style='light' />
             <Stack
                 screenOptions={{
                     headerStyle: { backgroundColor: 'black' },
