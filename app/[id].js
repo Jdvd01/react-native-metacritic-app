82fb273a43cb64b94f1c222d644f647607da0873
Jose Velasquez <velasquezdenorajose@gmail.com>
2024-11-17 19:16:39 -0400

    ✨ Adds first dynamic screen

---
+++ app/[id].js
@@ -0,0 +1,70 @@
+import { useState, useEffect } from 'react'
+import { Stack, useLocalSearchParams } from "expo-router";
+import { ActivityIndicator, Image, ScrollView, Text, View } from "react-native";
+import { getCharacterDetail } from '../lib/api';
+import Screen from "../components/Screen";
+import { Status } from '../components/Status';
+
+export default function Detail() {
+    const { id } = useLocalSearchParams()
+    const [characterDetail, setCharacterDetail] = useState(null)
+
+    useEffect(() => {
+        getCharacterDetail(id).then(data => setCharacterDetail(data))
+    }, [id])
+
+    return (
+        <Screen>
+            <Stack.Screen
+                options={{
+                    headerStyle: { backgroundColor: 'black' },
+                    headerTintColor: 'white',
+                    headerLeft: () => { },
+                    headerTitle: characterDetail?.name ?? `Character #${id}`,
+                    headerRight: () => { }
+                }}
+            />
+
+            <View>
+                {characterDetail == null ? (
+                    <ActivityIndicator color={'#fff'} size='large' />
+                ) : (
+                    <ScrollView>
+                        <View className='justify-center items-center'>
+                            <Image
+                                className='mb-4 rounded'
+                                source={{ uri: characterDetail.image }}
+                                style={{ width: 250, height: 300 }}
+                            />
+
+                            <Text className='text-white text-center font-bold mb-8 text-2xl'>
+                                {characterDetail.name}
+                            </Text>
+
+                            <View className='w-3/4'>
+                                <Status status={characterDetail.status} fontSize='text-xl' />
+                                <Text className='text-white text-xl'>
+                                    Species: {characterDetail.species}
+                                </Text>
+                                <Text className='text-white text-xl'>
+                                    Gender: {characterDetail.gender}
+                                </Text>
+                                {characterDetail.type.trim() != "" ? (
+                                    <Text className='text-white text-xl'>
+                                        Type: {characterDetail.type}
+                                    </Text>
+                                ) : null}
+                                <Text className='text-white text-xl'>
+                                    Origin: {characterDetail.origin?.name}
+                                </Text>
+                                <Text className='text-white text-xl'>
+                                    Location: {characterDetail.location?.name}
+                                </Text>
+                            </View>
+                        </View>
+                    </ScrollView>
+                )}
+            </View>
+        </Screen>
+    )
+}
--- components/CharacterCard.jsx
+++ components/CharacterCard.jsx
@@ -1,21 +1,27 @@
 import { useEffect, useRef } from "react";
-import { Animated, Image, Text, View } from "react-native"
+import { Animated, Image, Pressable, Text, View } from "react-native"
 import { Status } from "./Status";
+import { Link } from "expo-router";
 
 export function CharacterCard({ character }) {
     return (
-        <View className='flex-row gap-4 mb-5'>
-            <Image
-                className='w-[100] h-[100] rounded-xl'
-                source={{ uri: character.image }}
-            />
-            <View className='flex items-start'>
-                <Text className='text-2xl font-bold text-white'>{character.name}</Text>
-                <Text className='text-base text-white'>Specie: {character.species}</Text>
+        <Link href={`/${character.id}`} asChild>
+            <Pressable className='active:opacity-70 border border-black active:border-white/50 mb-2 rounded-xl p-1'>
 
-                <Status status={character.status} />
-            </View>
-        </View>
+                <View className='flex-row gap-4'>
+                    <Image
+                        className='w-[100] h-[100] rounded-xl'
+                        source={{ uri: character.image }}
+                    />
+                    <View className='flex items-start flex-shrink'>
+                        <Text className='text-2xl font-bold text-white'>{character.name}</Text>
+                        <Text className='text-base text-white'>Specie: {character.species}</Text>
+
+                        <Status status={character.status} />
+                    </View>
+                </View>
+            </Pressable>
+        </Link>
     )
 }
 
