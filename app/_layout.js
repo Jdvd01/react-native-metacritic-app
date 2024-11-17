a581452931e4e8113b60c4dec4bf7c9d0aafcf57
Jose Velasquez <velasquezdenorajose@gmail.com>
2024-11-17 19:12:31 -0400

    ✨ Adds Stack for my screens

--- app/_layout.js
+++ app/_layout.js
@@ -1,11 +1,20 @@
-import { Slot } from "expo-router"
+import { Stack } from "expo-router"
 import { View } from "react-native"
+import { Logo } from "../components/Icons"
 import '../global.css'
 
 export default function Layout() {
     return (
-        <View>
-            <Slot />
+        <View className='flex-1'>
+            <Stack
+                screenOptions={{
+                    headerStyle: { backgroundColor: 'black' },
+                    headerTintColor: 'white',
+                    headerTitle: '',
+                    headerLeft: () => <Logo color='white' size="50" />,
+                    headerRight: () => { }
+                }}
+            />
         </View>
     )
 }
